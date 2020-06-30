517141643b2681c6da5d389601ff3df6afee1faa
emi <emiliorizzo@gmail.com>
2020-06-29 23:01:27 -0600

    Update dataSource, create collections as default

--- src/lib/dataSource.js
+++ src/lib/dataSource.js
@@ -1,5 +1,13 @@
 
 import Setup from './Setup'
-export const setup = ({ log, skipCheck } = {}) => Setup({ log } = {}).then(setup => setup.start(skipCheck))
+export const setup = async ({ log, skipCheck, skipCollections } = {}) => {
+  try {
+    const setup = await Setup({ log } = {})
+    if (!skipCollections) await setup.createCollections()
+    return setup.start(skipCheck)
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
 export const dataSource = setup
 export default setup
